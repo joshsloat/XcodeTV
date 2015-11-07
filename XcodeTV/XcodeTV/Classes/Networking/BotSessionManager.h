ca1aae1359eddc6cc06c81c91d0ca309b2a98c09
Josh Sloat <jsloat@articulate.com>
2015-11-06 22:10:54 -0500

    Added ATS and AF workarounds for the server using a self-signed cert.

---
+++ XcodeTV/XcodeTV/Classes/Networking/BotSessionManager.h
@@ -0,0 +1,13 @@
+//
+//  BotSessionManager.h
+//  XcodeTV
+//
+//  Created by Josh Sloat on 11/6/15.
+//  Copyright © 2015 Articulate. All rights reserved.
+//
+
+#import "AFHTTPSessionManager.h"
+
+@interface BotSessionManager : AFHTTPSessionManager
+
+@end
